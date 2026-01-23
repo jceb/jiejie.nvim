de93704eb443ebbcbfc43d5f711454a9e85222aa
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2026-01-23 09:28:17 +0100

    fix: stop truncation of change description

--- lua/jiejie/log.lua
+++ lua/jiejie/log.lua
@@ -11,7 +11,7 @@ local M = {}
 --- @param callback fun(ctx: Context) Asynchronous callback
 M.load = function(ctx, callback)
   local template =
-    'change_id.shortest() ++ "\t" ++ "†" ++ if(empty, "(empty) ") ++ "‡" ++ if(description.first_line().len() == 0, "(no description set)", truncate_end(50, description.first_line(), "…")) ++ "⌠" ++ if(bookmarks.len() > 0, " " ++ bookmarks) ++ "⌡" ++ if(tags.len() > 0, " " ++ tags) ++ "∫" ++ if(git_head, " git_head()") ++ "∬" ++ if(conflict, " conflict") ++ "∮" ++ if(immutable, " immutable") ++ "∴" ++ change_id'
+    'change_id.shortest() ++ "\t" ++ "†" ++ if(empty, "(empty) ") ++ "‡" ++ if(description.first_line().len() == 0, "(no description set)", description.first_line()) ++ "⌠" ++ if(bookmarks.len() > 0, " " ++ bookmarks) ++ "⌡" ++ if(tags.len() > 0, " " ++ tags) ++ "∫" ++ if(git_head, " git_head()") ++ "∬" ++ if(conflict, " conflict") ++ "∮" ++ if(immutable, " immutable") ++ "∴" ++ change_id'
   local args = {
     "log",
     "-n",
