f6c58be750d1c11c6d25607e0afa49943ee83641
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2026-05-11 10:40:22 +0200

    Revert "fix: move dirty checking outside of vim.schedule to prevent issues with unset variables"
    This reverts commit ba550f25934b1a23193569c9a8dba6604826b420.

--- lua/jiejie/log_dirty_check.lua
+++ lua/jiejie/log_dirty_check.lua
@@ -82,26 +82,34 @@ end
 --- Mark everything dirty to trigger a reload
 --- @param buf number Buffer ID
 function M.dirty_mark_everything(buf)
-  set_dirty(buf, bit.bor(M.DIRTY_CONTENT, M.DIRTY_CURSOR))
+  vim.schedule(function()
+    set_dirty(buf, bit.bor(M.DIRTY_CONTENT, M.DIRTY_CURSOR))
+  end)
 end
 
 --- Run the autocommand that triggers a dirty check
 function M.do_dirty_check()
-  vim.cmd.doau("User JiejieDirtyCheck")
+  vim.schedule(function()
+    vim.cmd.doau("User JiejieDirtyCheck")
+  end)
 end
 
 --- Mark buffer content as dirty to trigger a reload
 --- @param buf number Buffer ID
 function M.dirty_mark_content(buf)
-  set_dirty(buf, M.DIRTY_CONTENT)
+  vim.schedule(function()
+    set_dirty(buf, M.DIRTY_CONTENT)
+  end)
 end
 
 --- Mark cursor position as dirty to trigger a reload
 --- @param ctx Context context
 function M.dirty_mark_cursor(ctx)
-  if set_dirty(ctx.buf, M.DIRTY_CURSOR) then
-    vim.api.nvim_buf_set_var(ctx.buf, "jiejie_dirty_cursor", ctx.curpos)
-  end
+  vim.schedule(function()
+    if set_dirty(ctx.buf, M.DIRTY_CURSOR) then
+      vim.api.nvim_buf_set_var(ctx.buf, "jiejie_dirty_cursor", ctx.curpos)
+    end
+  end)
 end
 
 --- Setup dirty checking for current buffer
